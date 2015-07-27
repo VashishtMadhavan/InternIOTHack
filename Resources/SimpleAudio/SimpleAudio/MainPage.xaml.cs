fdc693e74d863dc2df6d452f46eac7bc207f4d01
Alex Danilychev <t-alexad@microsoft.com>
2015-07-27 14:13:27 -0700

    Copied over all example resources

---
+++ Resources/SimpleAudio/SimpleAudio/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Storage.Pickers;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SimpleAudio
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+ 
+        public bool playing = false;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (playing) { 
+                this.HelloMessage.Text = "Not Playing";
+                playing = false;
+            }
+            else
+            {
+                this.HelloMessage.Text = "Playing";
+                playing = true;
+            }
+        }
+        private void PlayMusic()
+        {
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".mp3");
+        }
+    }
+    
+}
